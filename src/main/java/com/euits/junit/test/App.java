1fe00bf2004451f8d0f6e0ecc558ec62750c3e5d
User <User@User-PC9237>
2018-03-19 09:28:41 +0900

    opl9ipiopoi

--- src/main/java/com/euits/junit/test/App.java
+++ src/main/java/com/euits/junit/test/App.java
@@ -1,9 +1,17 @@
 package com.euits.junit.test;
 public class App {
+	
+	// 주석 1
+	// 주석 2
     public int sum(int i, int j) {
         return i+j;
     }
 
+    /*
+     * 주석 3
+     * 주석 4
+     * 주석 5
+     */
     public int min(int i, int j) {
         return i-j;
     }
